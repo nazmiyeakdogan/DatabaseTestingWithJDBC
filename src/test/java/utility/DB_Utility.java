c8a4c441a5fe9a98a3026a8c9de389ee676eeb50
akdoganflow <akdoganflow@gmail.com>
2023-04-26 23:27:13 +0300

    DB_Utility eklendi

---
+++ src/test/java/utility/DB_Utility.java
@@ -0,0 +1,44 @@
+package utility;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_Utility {
+
+    public static Connection connection;
+    public static Statement statement;
+
+
+    // Connection bağlantı için eklendi
+    @BeforeClass
+    public void DBConnectionOpen()
+    {
+        String hostUrl = "jdbc:mysql://db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com:3306/sakila";
+        String username = "root";
+        String password = "'\"-LhCB'.%k[4S]z";
+
+        try {
+            connection = DriverManager.getConnection(hostUrl, username, password); // bağlantıya click  1.madde
+            statement = connection.createStatement(); // sorguları çalıştırmak için sorgu gönderme ortamı
+        }catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    // Connection işlem sonrası kapatılması için eklendi
+    @AfterClass
+    public void DBConnectionClose(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
